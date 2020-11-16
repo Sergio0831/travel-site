a8b42d690a111abda273b6e865d98d2c54c56bda
Sergejs Ivcenko <sergejs.ivcenko@gmail.com>
2020-11-16 09:32:32 +0000

    Completed sticky header and smooth scroll.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
 import ReaveaOnScroll from './modules/RevealOnScroll'
+import StickyHeader from './modules/StickyHeader'
 
+let stickyHeader = new StickyHeader();
 let mobileMenu =  new MobileMenu();
 new ReaveaOnScroll(document.querySelectorAll(".feature-item"), 75);
 new ReaveaOnScroll(document.querySelectorAll(".testimonial"), 60);
